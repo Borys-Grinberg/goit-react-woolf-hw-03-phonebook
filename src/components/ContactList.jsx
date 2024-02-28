37020e9958b0e574338dc4f86946cb395e45050b
Borys-Grinberg <borys.grinberg@gmail.com>
2024-02-28 19:50:16 +0100

    deploy

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import styles from './ContactList.module.css';
 
 const ContactList = ({ contacts, deleteContact }) => {
   return (
     <ul>
       {contacts.map(contact => (
-        <li key={contact.id}>
+        <li className={styles.li} key={contact.id}>
           {contact.name}: {contact.number}
-          <button onClick={() => deleteContact(contact.id)}>Delete</button>
+          <button
+            className={styles.button}
+            onClick={() => deleteContact(contact.id)}
+          >
+            Delete
+          </button>
         </li>
       ))}
     </ul>
